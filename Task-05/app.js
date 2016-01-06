6c792d90b9bdf82b7d6581ae1991e02b0067a350
Alina Mieliekh <alina.mieliekh@gmail.com>
2016-01-06 17:24:32 +0100

    Task-05

---
+++ Task-05/app.js
@@ -0,0 +1,7 @@
+var el = document.getElementsByName("quantity")[0];
+el.addEventListener("keyup", function (event) {
+    var words = toWords(el.value);
+    document.getElementById("tx").value = words;
+    }, true);
+
+
